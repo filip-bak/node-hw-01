c51c44e6c6de74613ad612cf8f6f00b783947329
Filip Bączkowski <filo.b@wp.pl>
2023-09-10 21:56:46 +0200

    feat: add functionality

--- contacts.js
+++ contacts.js
@@ -1,1 +1,103 @@
-console.log("Test Contacts");
+const fs = require("fs");
+const path = require("path");
+const { nanoid } = require("nanoid");
+const colors = require("./utils/colors");
+
+const { consoleTable } = require("./utils/terminal");
+
+const contactsPath = path.resolve(__dirname, "./db/contacts.json");
+
+function listContacts() {
+  fs.readFile(contactsPath, "utf8", (err, data) => {
+    if (err) {
+      console.log(colors.error(`Reading error: ${err.message}`));
+      return;
+    }
+    const contacts = JSON.parse(data);
+
+    consoleTable("Contacts list:", contacts);
+  });
+}
+
+function getContactById(contactId) {
+  fs.readFile(contactsPath, "utf8", (err, data) => {
+    if (!contactId) return;
+    if (err) {
+      console.log(colors.error(`Reading error: ${err.message}`));
+      return;
+    }
+    const contacts = JSON.parse(data);
+
+    const contactById = contacts.find(({ id }) => id === contactId);
+
+    if (!contactById) {
+      console.log(
+        colors.error(`Contact with ID ${colors.data(contactId)} was not found.`)
+      );
+      return;
+    }
+
+    consoleTable("Contact by id:", [contactById]);
+  });
+}
+
+function removeContact(contactId) {
+  if (!contactId) return;
+  fs.readFile(contactsPath, "utf8", (err, data) => {
+    if (err) {
+      console.log(colors.error(`Reading error: ${err.message}`));
+      return;
+    }
+    const contacts = JSON.parse(data);
+
+    const contactIndex = contacts.findIndex(({ id }) => id === contactId);
+
+    if (contactIndex === -1) {
+      console.log(
+        colors.error(`Contact with ID ${colors.data(contactId)} was not found.`)
+      );
+      return;
+    }
+
+    const removedContact = contacts.splice(contactIndex, 1);
+
+    fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2), (err) => {
+      if (err) {
+        console.log(colors.error(`Reading error: ${err.message}`));
+        return;
+      }
+
+      consoleTable("Contact removed successfully.", removedContact);
+    });
+  });
+}
+
+function addContact(name, email, phone) {
+  fs.readFile(contactsPath, "utf8", (err, data) => {
+    if (err) {
+      console.log(colors.error(`Reading error: ${err.message}`));
+      return;
+    }
+    const contacts = JSON.parse(data);
+
+    const newContact = { id: nanoid(), name, email, phone };
+    contacts.push(newContact);
+
+    fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2), (err) => {
+      if (err) {
+        console.log(colors.error(`Reading error: ${err.message}`));
+        return;
+      }
+
+      consoleTable(`Contact added successfully.`, [newContact]);
+    });
+  });
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  colors,
+};
--- index.js
+++ index.js
@@ -1,1 +1,61 @@
-import "./contacts.js";
+const {
+  addContact,
+  getContactById,
+  listContacts,
+  removeContact,
+} = require("./contacts");
+const { Command } = require("commander");
+
+const colors = require("./utils/colors");
+const { helperMessage } = require("./utils/terminal");
+
+const program = new Command();
+program
+  .requiredOption(
+    "-a, --action <type>",
+    'choose action (choices: "list", "get", "add", "remove")'
+  )
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+const argv = program.opts();
+
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      listContacts();
+      break;
+
+    case "get":
+      if (!id) {
+        helperMessage("get");
+        return;
+      }
+      getContactById(id);
+      break;
+
+    case "add":
+      if (!name || !email || !phone) {
+        helperMessage("add");
+        return;
+      }
+      addContact(name, email, phone);
+      break;
+
+    case "remove":
+      if (!id) {
+        helperMessage("remove");
+        return;
+      }
+      removeContact(id);
+      break;
+
+    default:
+      console.log(colors.error("Unknown action type!"));
+  }
+}
+
+invokeAction(argv);
